41f9328c4193296b5bdb4f55fa73e2c27d6b3786
Leventbk <leventbkarakus@hotmail.com>
2023-07-19 15:35:33 -0400

    remove hard data from client.js and class changed to id on new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function (tweets) {
-  const $tweetContainer = $('.tweet-container');
+  const $tweetContainer = $('#main-tweet-container');
   $tweetContainer.empty();
 
   tweets.forEach(tweet => {        // calls createTweetElement for each tweet
@@ -54,16 +28,19 @@ const createTweetElement = function (tweet) {
   // $header.append($avatars, $name)
 
   const $tweet = $(`
-
     <section class="tweet-container">
       <article>
-        <header class="tweet-container-header">
-          <img src="${escape(tweet.user.avatars)}"/>
+        <header class=tweet-head>
+          <div class="user-data">
+            <img src="${escape(tweet.user.avatars)}"/>
             <p>${escape(tweet.user.name)}</p>
-            <p class="tcb-text">${tweet.user.handle}</p>
+          </div>
+          <div >
+            <span class="user-handle">${escape(tweet.user.handle)}</span>
+          </div>
         </header>
         <div class="tweet-container-body">
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
         </div>
         <footer class="tweet-container-footer">
           <div class="tcf-time">
